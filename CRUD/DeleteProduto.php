cd8a58742f5dd91b144cc5d6264076bb8a8499ae
Gladson Simões <gladsonssimoes@outlook.com.br>
2023-04-14 15:00:34 -0300

    Create DeleteProduto.php

---
+++ CRUD/DeleteProduto.php
@@ -0,0 +1,17 @@
+<?php
+    require('../conn.php');
+
+   if( isset($_GET['produto'])){
+        $produto = $_GET['produto'];
+   }else{
+        header("Location: ../index.php");
+   }
+
+   $del_prod = $pdo->prepare('DELETE FROM produtos WHERE id_produto=:id_produto;');
+   $del_prod->execute(array(':id_produto'=>$produto));  
+
+   echo "<script>
+   alert('Usuário Deletado!');
+   window.location.href='../tabela.php';
+   </script>";
+?>

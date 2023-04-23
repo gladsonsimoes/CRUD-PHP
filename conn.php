567137c5787fe30ae4129a8fe523c5ac89703417
gladsonssimoes <gladsonssimoes@outlook.com.br>
2023-04-23 17:18:26 -0300

    Troquei o dbname pois é diferente

--- conn.php
+++ conn.php
@@ -1,10 +1,11 @@
 <?php
 
 try{
-    $pdo = new PDO('mysql:host=localhost;dbname=metalcoin;port=3312;','root','');
+    $pdo = new PDO('mysql:host=localhost;dbname=dbcrud;port=3312;','root','');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->exec("SET CHARACTER SET utf8");
 } catch(PDOException $e) {
     echo "Error". $e->getMessage();
+   
 }
 ?>

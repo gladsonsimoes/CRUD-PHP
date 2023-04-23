54b7688e849dd86d8afb1da709f579f4f707a3cc
gladsonssimoes <gladsonssimoes@outlook.com.br>
2023-04-23 17:19:21 -0300

    Algumas mudanças no update

--- CRUD/UpdateProduto.php
+++ CRUD/UpdateProduto.php
@@ -11,10 +11,10 @@ if (empty($id_produto) || empty($nome_produto) || empty($qtd_produto) || empty($
     echo "Os valores não podem ser vazios";
 } else {
     $update_prod = $pdo->prepare("UPDATE produtos set 
-        (nome_produto = :nome_produto, 
+        nome_produto = :nome_produto, 
         qtd_produto = :qtd_produto, 
         valor_produto = :valor_produto, 
-        cat_produto = :cat_produto) WHERE id_produto = :id_produto;");
+        cat_produto = :cat_produto WHERE id_produto = :id_produto");
 
     $update_prod->execute(
         array(

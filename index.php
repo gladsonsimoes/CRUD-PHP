ff7ba8ca938a9a19a4ab0fff108e68c598c9441a
gladsonssimoes <gladsonssimoes@outlook.com.br>
2023-04-23 17:20:13 -0300

    alteração no action

--- index.php
+++ index.php
@@ -12,26 +12,26 @@
         <br>
         <h1> Cadastro de Produtos</h1>
         <br>
-        <form action="CRUD/cadastrar_produto.php" method="post">
+        <form action="CRUD/CreateProduto.php" method="post">
             <div>
                 <label>Nome Produto: </label>
-                <input type="text" name="nome_produto">
+                <input type="text" name="nome_produto" required>
             </div>
             <div>
                 <label>Quantidade do Produto: </label>
-                <input type="text" name="qtd_produto">
+                <input type="text" name="qtd_produto" required>
             </div>
             <div>
                 <label>Valor do Produto: </label>
-                <input type="text" name="valor_produto">
+                <input type="text" name="valor_produto" required>
             </div>
             <div>
                 <label>Categoria do Produto: </label>
-                <input type="text" name="cat_produto">
+                <input type="text" name="cat_produto" required>
             </div>
             <div>
                 <button type="submit">Enviar Produtos</button>
-                <a href="tabela.php"> Ver Produtos </a>
+                <a href="ReadProduto.php"> Ver Produtos </a>
             </div>
         </form>
     </div>
